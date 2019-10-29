10e3160f99797f4df9699eadc3c88f954aa47d35
Rahul Prasad <rahul.prsd.in@gmail,com>
2019-10-29 18:59:55 +0800

    Style

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,16 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-        id: 't1', 
-        title: 'new shoes', 
-        amount: 2000,
-        date: DateTime.now(),
+      id: 't1',
+      title: 'new shoes',
+      amount: 2000,
+      date: DateTime.now(),
     ),
     Transaction(
-        id: 't2', 
-        title: 'dinner', 
-        amount: 2000, 
-        date: DateTime.now(),
+      id: 't2',
+      title: 'dinner',
+      amount: 2000,
+      date: DateTime.now(),
     ),
   ];
 
@@ -49,8 +49,52 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Column(
-            children: transactions.map((transaction){
-              return Card(child: Text(transaction.title),);
+            children: transactions.map((transaction) {
+              return Card(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    child: Text(
+                      transaction.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.black,
+                       ),
+                      ),
+                      Text(
+                        transaction.date.toString(),
+                        style: TextStyle(
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ));
             }).toList(),
           )
         ],
